d6d372dd6103af79a587f09361598247e1f4601c
bitanyadereje <bithanyaa.dereje@gmail.com>
2026-08-14 08:17:24 +0300

    worldcities app with ASP.NET AND ANGULAR City and Country entities with EF Core - SQL Server database with migrations - Seeded 44,691 cities and 240 countries - CitiesController API endpoint - Angular CitiesComponent with HttpClient - Cities table with loading state - Angular Material table with pagination

---
+++ WorldCities.Server/Data/Models/City.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WorldCities.Server.Data.Models
+{
+    [Table("Cities")]
+    [Index(nameof(Name))]
+    [Index(nameof(Lat))]
+    [Index(nameof(Lon))]
+    public class City
+    {
+        #region Properties
+       
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        
+        public required string Name { get; set; }
+
+      
+        [Column(TypeName = "decimal(7,4)")]
+        public decimal Lat { get; set; }
+
+        
+        [Column(TypeName = "decimal(7,4)")]
+        public decimal Lon { get; set; }
+
+        [ForeignKey(nameof(Country))]
+        public int CountryId { get; set; }
+        #endregion
+
+        #region Navigation Properties
+       
+        public Country? Country { get; set; }
+        #endregion
+    }
+}
